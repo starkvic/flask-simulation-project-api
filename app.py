1746701948bc5dbac55b0f518287a3b3f04ffe68
starkvic <starkvic@github.com>
2025-04-05 02:13:59 +0300

    Changed deployment type from local server to gunicorn to run on website

--- app.py
+++ app.py
@@ -20,7 +20,4 @@ def simulate():
     else:
         return jsonify({"error": "Unsupported algorithm"}), 400
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+#Gunicorn - handles the server
