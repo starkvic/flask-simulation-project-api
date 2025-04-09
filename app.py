2a1df5fb5d58334eb7cf623fff0ea62f9081b196
starkvic <starkvic@github.com>
2025-04-09 10:06:24 +0300

    The referencing for the hippopotamus algorithm was wrong thus updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from simulation.hippopotamus import run_hippo_algorithm
+from simulation.algorithms.hippopotamus import run_hippo_algorithm
 
 app = Flask(__name__)
 CORS(app)
